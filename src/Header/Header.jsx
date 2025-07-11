caf892b46ea26086918a57a9d976ef7445573342
HungDuong2909 <hungduongg2909@gmail.com>
2025-07-11 10:43:44 +0700

    add btn login

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,32 +1,33 @@
-import React from 'react';
-import { AuthContext } from '../Context/AuthContext';
+import React from "react";
+import { AuthContext } from "../Context/AuthContext";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import Logoicon from '../Image/logo-icon.png';
-import Logotext from '../Image/logo-text.png';
-import Logolight from '../Image/logo-light-text.png';
+import Logoicon from "../Image/logo-icon.png";
+import Logotext from "../Image/logo-text.png";
+import Logolight from "../Image/logo-light-text.png";
 
 function Header(props) {
-	const { user } = useContext(AuthContext);
-	const { loading, error, dispatch } = useContext(AuthContext);
+   const { user } = useContext(AuthContext);
+   const { loading, error, dispatch } = useContext(AuthContext);
 
-	const handleLogout = () => {
-		dispatch("LOGOUT");
-		localStorage.removeItem("user");
-	}
+   const handleLogout = () => {
+      dispatch("LOGOUT");
+      localStorage.removeItem("user");
+   };
 
-	return (
-		<header className='topbar' data-navbarbg='skin6'>
-			<nav className='navbar top-navbar navbar-expand-md'>
-				<div className='navbar-header' data-logobg='skin6'>
-					<a
-						className='nav-toggler waves-effect waves-light d-block d-md-none'
-						href='#'>
-						<i className='ti-menu ti-close'></i>
-					</a>
-					<div className='navbar-brand'>
-						<Link to='/'>
-							{/* <b className='logo-icon'>
+   return (
+      <header className="topbar" data-navbarbg="skin6">
+         <nav className="navbar top-navbar navbar-expand-md">
+            <div className="navbar-header" data-logobg="skin6">
+               <a
+                  className="nav-toggler waves-effect waves-light d-block d-md-none"
+                  href="#"
+               >
+                  <i className="ti-menu ti-close"></i>
+               </a>
+               <div className="navbar-brand">
+                  <Link to="/">
+                     {/* <b className='logo-icon'>
 								<img
 									src={Logoicon}
 									alt='homepage'
@@ -38,8 +39,8 @@ function Header(props) {
 									className='light-logo'
 								/>
 							</b> */}
-							<span className='logo-text'>
-								{/* <img
+                     <span className="logo-text">
+                        {/* <img
 									src={Logotext}
 									alt='homepage'
 									className='dark-logo'
@@ -49,11 +50,11 @@ function Header(props) {
 									className='light-logo'
 									alt='homepage'
 								/> */}
-								<span>Admin Page</span>
-							</span>
-						</Link>
-					</div>
-					{/* <a
+                        <span>Admin Page</span>
+                     </span>
+                  </Link>
+               </div>
+               {/* <a
 						className='topbartoggler d-block d-md-none waves-effect waves-light'
 						href='#'
 						data-toggle='collapse'
@@ -63,13 +64,14 @@ function Header(props) {
 						aria-label='Toggle navigation'>
 						<i className='ti-more'></i>
 					</a> */}
-				</div>
-				<div
-					className='navbar-collapse collapse'
-					id='navbarSupportedContent'>
-					<ul className='navbar-nav float-left mr-auto ml-3 pl-1'>
-						<li className='nav-item dropdown'>
-							{/* <a
+            </div>
+            <div
+               className="navbar-collapse collapse"
+               id="navbarSupportedContent"
+            >
+               <ul className="navbar-nav float-left mr-auto ml-3 pl-1">
+                  <li className="nav-item dropdown">
+                     {/* <a
 								className='nav-link dropdown-toggle'
 								href='#'
 								id='navbarDropdown'
@@ -79,58 +81,77 @@ function Header(props) {
 								aria-expanded='false'>
 								<i data-feather='settings' className='svg-icon'></i>
 							</a> */}
-							<div
-								className='dropdown-menu'
-								aria-labelledby='navbarDropdown'>
-								<a className='dropdown-item' href='#'>
-									Action
-								</a>
-								<a className='dropdown-item' href='#'>
-									Another action
-								</a>
-								<div className='dropdown-divider'></div>
-								<a className='dropdown-item' href='#'>
-									Something else here
-								</a>
-							</div>
-						</li>
-					</ul>
-					<ul className='navbar-nav float-right'>
-						<li className='nav-item dropdown'>
-							<a
-								className='nav-link dropdown-toggle'
-								href='#'
-								data-toggle='dropdown'
-								aria-haspopup='true'
-								aria-expanded='false'>
-								{/* <img
+                     <div
+                        className="dropdown-menu"
+                        aria-labelledby="navbarDropdown"
+                     >
+                        <a className="dropdown-item" href="#">
+                           Action
+                        </a>
+                        <a className="dropdown-item" href="#">
+                           Another action
+                        </a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">
+                           Something else here
+                        </a>
+                     </div>
+                  </li>
+               </ul>
+               <ul className="navbar-nav float-right">
+                  <li className="nav-item dropdown">
+                     <a
+                        className="nav-link dropdown-toggle"
+                        href="/login"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                     >
+                        <span className="ml-2 d-none d-lg-inline-block">
+                           <span>Login</span>
+                        </span>
+                     </a>
+                  </li>
+                  <li className="nav-item dropdown">
+                     <a
+                        className="nav-link dropdown-toggle"
+                        href="#"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                     >
+                        {/* <img
 									src='../assets/images/users/IMG_6225.jpg'
 									alt='user'
 									className='rounded-circle'
 									width='40'
 								/> */}
-								<span className='ml-2 d-none d-lg-inline-block'>
-									<span>Hello,</span>{' '}
-									<span className='text-dark'>{user ? user.email : ''}</span>{' '}
-									<i
-										data-feather='chevron-down'
-										className='svg-icon'></i>
-								</span>
-							</a>
-							<div className='dropdown-menu dropdown-menu-right user-dd animated flipInY'>
-								<a className='dropdown-item' onClick={handleLogout}>
-									<i
-										data-feather='power'
-										className='svg-icon mr-2 ml-1'></i>
-									Logout
-								</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</header>
-	);
+                        <span className="ml-2 d-none d-lg-inline-block">
+                           <span>Hello,</span>{" "}
+                           <span className="text-dark">
+                              {user ? user.email : ""}
+                           </span>{" "}
+                           <i
+                              data-feather="chevron-down"
+                              className="svg-icon"
+                           ></i>
+                        </span>
+                     </a>
+                     <div className="dropdown-menu dropdown-menu-right user-dd animated flipInY">
+                        <a className="dropdown-item" onClick={handleLogout}>
+                           <i
+                              data-feather="power"
+                              className="svg-icon mr-2 ml-1"
+                           ></i>
+                           Logout
+                        </a>
+                     </div>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </header>
+   );
 }
 
 export default Header;
